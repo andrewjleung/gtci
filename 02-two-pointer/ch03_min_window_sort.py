2a7dcba3f031a1b99c3f89756528b9216e95dec7
Andrew Leung <andrewleung104@gmail.com>
2022-06-14 17:07:44 -0400

    Optimal min window sort solution

--- 02-two-pointer/ch03_min_window_sort.py
+++ 02-two-pointer/ch03_min_window_sort.py
@@ -6,28 +6,35 @@ array.
 
 def min_window_sort(arr):
     """
-    Time Complexity:  O(n*logn)
-    Space Complexity: O(n)
+    Time Complexity:  O(n)
+    Space Complexity: O(1)
     """
-    sorted_arr = sorted(arr)
-    min_window = 0
+    low = 0
+    high = len(arr) - 1
 
-    for left in range(len(arr)):
-        if arr[left] != sorted_arr[left]:
-            min_window -= left
-            break
+    while low < len(arr) - 1 and arr[low] < arr[low + 1]:
+        low += 1
 
-    for right in range(len(arr) - 1, -1, -1):
-        if arr[right] != sorted_arr[right]:
-            min_window += right + 1
-            break
+    if low >= len(arr) - 1:
+        return 0
 
-    return min_window
+    while high > 0 and arr[high - 1] < arr[high]:
+        high -= 1
+
+    window_max = max(arr[low:high + 1])
+    window_min = min(arr[low:high + 1])
+
+    while low > 0 and arr[low - 1] > min_window:
+        low -= 1
+
+    while high < len(arr) - 1 and arr[high + 1] < window_max:
+        high += 1
+
+    return high - low + 1
 
 
 def test_ex1():
     arr = [1, 2, 5, 3, 7, 10, 9, 12]
-    sorted_arr = [1, 2, 3, 5, 7, 9, 10, 12]
     o = 5
     assert min_window_sort(arr) == o
 
