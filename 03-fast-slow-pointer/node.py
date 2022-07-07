41f429778fa5b84bdec66cf65121f968ad99760f
Andrew Leung <andrewleung104@gmail.com>
2022-07-07 10:59:03 -0400

    Add node convenience constructor

--- 03-fast-slow-pointer/node.py
+++ 03-fast-slow-pointer/node.py
@@ -2,3 +2,28 @@ class Node:
     def __init__(self, value, next=None):
         self.value = value
         self.next = next
+
+    def structeq(self, other):
+        if other is None:
+            return False
+
+        return self.value == other.value and ((self.next is None and other.next is None) or (self.next.structeq(other.next)))
+
+
+def node(lst):
+    if len(lst) < 1:
+        return None
+
+    return Node(lst[0], node(lst[1:]))
+
+
+def test_empty():
+    assert node([]) == None
+
+
+def test_nonempty1():
+    assert node([1]).structeq(Node(1))
+
+
+def test_nonempty2():
+    assert node([1, 2, 3, 4]).structeq(Node(1, Node(2, Node(3, Node(4)))))
