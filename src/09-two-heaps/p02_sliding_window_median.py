399b787cf204479f6f0a9b903f6a257e8ceae4a7
Andrew Leung <andrewleung104@gmail.com>
2022-07-26 11:09:09 -0400

    Use `heapremove` function to avoid O(n) heapify

--- src/09-two-heaps/p02_sliding_window_median.py
+++ src/09-two-heaps/p02_sliding_window_median.py
@@ -1,4 +1,5 @@
-from heapq import (heappush, heappop, heapify)
+from heapq import (heappush, heappop, heapify, _siftdown, _siftup)
+import math
 
 """
 Given an array of numbers and a number `k`, find the median of all the `k` sized sub-arrays (or windows) of the array.
@@ -16,22 +17,19 @@ def sliding_window_median(nums, k):
 
     window_start = 0
 
-    for window_end in range(len(nums)):
+    for window_end in range(len(nums)):  # O(n)
         # Add the value at window_end to the heap.
         if len(max_heap) < 1 or nums[window_end] <= -max_heap[0]:
             heappush(max_heap, -nums[window_end])
         else:
             heappush(min_heap, nums[window_end])
 
-        # Remove the number going out of the window.
+        # Remove the number going out of the window once the window is at the right size.
         if window_end > k - 1:
             if nums[window_start] <= -max_heap[0]:
-                max_heap.remove(-nums[window_start])
-                # Supposedly there's a way to do this in O(logk) time rather than O(k) using `heapq` sift methods... Either way the overall runtime is the same because `remove` is O(k).
-                heapify(max_heap)
+                heapremove(max_heap, -nums[window_start])  # O(k)
             else:
-                min_heap.remove(nums[window_start])
-                heapify(min_heap)
+                heapremove(min_heap, nums[window_start])  # O(k)
 
             window_start += 1
 
@@ -41,7 +39,7 @@ def sliding_window_median(nums, k):
         elif len(min_heap) > len(max_heap):
             heappush(max_heap, -heappop(min_heap))
 
-        # Accumulate a median.
+        # Accumulate the median once the window is at the right size.
         if window_end >= k - 1:
             if len(max_heap) != len(min_heap):
                 results.append(-max_heap[0])
@@ -49,6 +47,30 @@ def sliding_window_median(nums, k):
                 results.append((-max_heap[0] + min_heap[0]) / 2.0)
 
     return results
+
+
+def heapremove(heap, element):
+    """
+    Time Complexity:  O(n)
+    Space Complexity: O(1)
+    """
+    # Find the index of the element.
+    i = heap.index(element)  # O(n)
+
+    # Remove the element by moving the last node of the heap into its place.
+    # This will violate the heap property, requiring you to re-heapify (not the whole heap however).
+    heap[i] = heap[-1]
+    del heap[-1]
+
+    # If the element was the last element, no heapification is required.
+    # Otherwise, re-heapify the single element.
+    if i >= len(heap):
+        return
+
+    # `heapify` could be used here, but that algorithm is O(n).
+    # Since only a single element needs to be adjusted, this can be done in O(logn).
+    _siftup(heap, i)  # O(logn)
+    _siftdown(heap, 0, i)  # O(logn)
 
 
 def test_ex1():
