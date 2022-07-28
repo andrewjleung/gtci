3406acc0c39bf1828fcaab1c959f8cdfedc4d93b
Andrew Leung <andrewleung104@gmail.com>
2022-07-28 11:58:47 -0400

    Add memoization to evaluate expression

--- src/10-subsets/ch01_evaluate_expression.py
+++ src/10-subsets/ch01_evaluate_expression.py
@@ -14,6 +14,18 @@ expression can be evaluated by grouping the numbers and operators using parenthe
 - Every ordering of execution may only appear once, even if there are different groupings involved.
 """
 
+"""
+Time Complexity:  O(n * cat(n))
+    - A grouped expression is just a binary tree. The number of expressions with distinct 
+    groupings that can be created from a given expression is just the number of binary trees 
+    with O(n) nodes (each operator is a node, an expression has O(n) operators).
+    - The number of binary trees with n nodes is the nth Catalan number.
+    - Each expression takes O(n) time to evaluate (copy the string, execute the operations).
+
+Space Complexity: O(cat(n))
+    - Storing the results for `cat(n)` evaluations.
+"""
+
 OPS = {
     "+": lambda l, r: l + r,
     "-": lambda l, r: l - r,
@@ -21,65 +33,74 @@ OPS = {
 }
 
 
-def do_cartesian_op(left_evals, op, right_evals):
-    evals = []
+class EvaluateAllExpressions:
+    def __init__(self):
+        self.memo = {}
+        self.hits = 0
+        self.calls = 0
 
-    for left in left_evals:
-        for right in right_evals:
-            op_fn = OPS[op]
-            evals.append(op_fn(left, right))
+    def do_cartesian_op(self, left_evals, op, right_evals):
+        evals = []
 
-    return evals
+        for left in left_evals:
+            for right in right_evals:
+                op_fn = OPS[op]
+                evals.append(op_fn(left, right))
 
+        return evals
 
-def evaluate(expr):
-    """
-    Time Complexity:  O(n * cat(n))
-        - A grouped expression is just a binary tree. The number of expressions with distinct 
-          groupings that can be created from a given expression is just the number of binary trees 
-          with O(n) nodes (each operator is a node, an expression has O(n) operators).
-        - The number of binary trees with n nodes is the nth Catalan number.
-        - Each expression takes O(n) time to evaluate (copy the string, execute the operations).
+    def evaluate_all_expressions(self, expr):
 
-    Space Complexity: O(cat(n))
-        - Storing the results for `cat(n)` evaluations.
-    """
-    # Base case. An expression of a single number evaluates to itself.
-    if len(expr) == 1:
-        return [int(expr)]
+        # Use memoization!
+        if expr in self.memo:
+            self.hits += 1
+            return self.memo[expr]
 
-    evals = []
+        self.calls += 1
+        # Base case. An expression of a single number evaluates to itself.
+        if len(expr) == 1:
+            return [int(expr)]
 
-    # Iterate through every char looking for operators.
-    for i in range(len(expr)):
-        # Assuming well-formedness, anything that isn't a number is an operator.
-        if not expr[i].isnumeric():
-            # Recursively evaluate the left and right expressions relative to this operator.
-            left_evals = evaluate(expr[:i])
-            right_evals = evaluate(expr[i + 1:])
+        evals = []
 
-            # Calculate the Cartesian product of the left and right evaluations.
-            evals += do_cartesian_op(left_evals, expr[i], right_evals)
+        # Iterate through every char looking for operators.
+        for i in range(len(expr)):
+            # Assuming well-formedness, anything that isn't a number is an operator.
+            if not expr[i].isnumeric():
+                # Recursively evaluate the left and right expressions relative to this operator.
+                left_evals = self.evaluate_all_expressions(expr[:i])
+                right_evals = self.evaluate_all_expressions(expr[i + 1:])
 
-    return evals
+                # Calculate the Cartesian product of the left and right evaluations.
+                evals += self.do_cartesian_op(left_evals, expr[i], right_evals)
+
+        self.memo[expr] = evals
+
+        return evals
 
 
 def test_ex1():
     expr = "1+2*3"
-    actual = evaluate(expr)
+    e = EvaluateAllExpressions()
+    actual = e.evaluate_all_expressions(expr)
     expected = [7, 9]
+    print(f"calls: {e.calls}\nhits:  {e.hits}")
     assert contains_same_elements(actual, expected)
 
 
 def test_ex2():
     expr = "2*3-4-5"
-    actual = evaluate(expr)
+    e = EvaluateAllExpressions()
+    actual = e.evaluate_all_expressions(expr)
     expected = [8, -12, 7, -7, -3]
+    print(f"calls: {e.calls}\nhits:  {e.hits}")
     assert contains_same_elements(actual, expected)
 
 
 def test_ex3():
     expr = "1+1*1"
-    actual = evaluate(expr)
+    e = EvaluateAllExpressions()
+    actual = e.evaluate_all_expressions(expr)
     expected = [2, 2]
+    print(f"calls: {e.calls}\nhits:  {e.hits}")
     assert contains_same_elements(actual, expected)
